99473993c5ce808b7aaa7f4611869285d0657d80
nicolas <nicolas.dsw@gmail.com>
2022-07-17 20:45:11 -0300

    🔥 - Remove webjars

--- src/main/java/br/com/firedev/web/HomePage.java
+++ src/main/java/br/com/firedev/web/HomePage.java
@@ -1,14 +1,10 @@
 package br.com.firedev.web;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.firedev.web.worklogs.WorklogController;
-import br.com.firedev.web.worklogs.WorklogFilter;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -16,17 +12,9 @@ import lombok.AllArgsConstructor;
 @RequestMapping({ "/" })
 public class HomePage {
 
-	private final WorklogController worklogController;
-
 	@GetMapping
 	public String index(ModelMap mm) {
-		mm.addAttribute("welcome", "Olá");
+		mm.addAttribute("welcome", "Olá Mundo!");
 		return "index";
-	}
-
-	@RequestMapping("/worklogs")
-	public String list(ModelMap model, WorklogFilter filter, @SortDefault("author") Pageable pageable) {
-		model.addAttribute("page", this.worklogController.index(filter, pageable));
-		return "worklogs/worklogs";
 	}
 }
